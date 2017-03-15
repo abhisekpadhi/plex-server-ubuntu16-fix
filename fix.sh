a9f66c8e2fb87ee47c29df31035b34ff35a9688b
abhisekpadhi <avicool000@gmail.com>
2017-03-15 21:43:19 +0530

    Fixed script to properly sed and replace strings from content.txt and output into plexmediaserver.service

--- fix.sh
+++ fix.sh
@@ -2,38 +2,30 @@
 # Credits: https://goo.gl/N4Tc2B
 
 ###### CONSTANTS ######
-target_file=`echo "/etc/systemd/system/plexmediaserver.service"`
+target_file=`echo "/etc/systemd/system/"`
 curr_usr=`echo $USER`
 curr_grp=$curr_usr
+fs=`du -k $target_file | cut -f1`
+sed "s/username/$curr_usr/;s/groupname/$curr_grp/" content.txt > plexmediaserver.service
 #######################
 
-fs=`du -k $target_file | cut -f1`
-
-# function step2 {
-#   sudo chown -R $curr_usr:$curr_grp /var/lib/plexmediaserver &> /dev/null
-#   sudo systemctl --system daemon-reload &> /dev/null
-#   sudo service plexmediaserver start &> /dev/null
-#   sudo service plexmediaserver restart &> /dev/null
-# }
-
-# sudo systemctl stop plexmediaserver.service &> /dev/null
-sudo service plexmediaserver stop &> /dev/null
+sudo systemctl stop plexmediaserver.service &> /dev/null
 
 if [ ! -f $target_file ]
   then
     sudo touch $target_file
-    sudo cp plexmediaserver.service.txt $target_file &> /dev/null
+    sudo cp plexmediaserver.service $target_file &> /dev/null
     sudo chown -R $curr_usr:$curr_grp /var/lib/plexmediaserver &> /dev/null
     sudo systemctl --system daemon-reload &> /dev/null
     sudo service plexmediaserver start &> /dev/null
 elif [ $fs -eq 0 ]
    then
-    sudo cp plexmediaserver.service.txt $target_file &> /dev/null
+    sudo cp plexmediaserver.service $target_file &> /dev/null
     sudo chown -R $curr_usr:$curr_grp /var/lib/plexmediaserver &> /dev/null
     sudo systemctl --system daemon-reload &> /dev/null
     sudo service plexmediaserver start &> /dev/null
 else
-  sudo cp plexmediaserver.service.txt $target_file &> /dev/null
+  sudo cp plexmediaserver.service $target_file &> /dev/null
   sudo chown -R $curr_usr:$curr_grp /var/lib/plexmediaserver &> /dev/null
   sudo systemctl --system daemon-reload &> /dev/null
   sudo service plexmediaserver start &> /dev/null
